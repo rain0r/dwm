1911b015f2213da9402726373f7e194a6b62ccb4
Rainer Hihn <hihn.rainer@gmail.com>
2019-02-20 18:35:16 +0100

    lock screen on super+l

--- config.h
+++ config.h
@@ -63,6 +63,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *lockcmd[]  = { "xscreensaver-command", "-lock", NULL };
 
 static Key keys[] = {
 	/* modifier                     key         function        argument */
@@ -75,7 +76,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_f,       setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_h,       setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_i,       incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_l,       setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_l,       spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_m,       setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_p,       spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_period,  focusmon,       {.i = +1 } },
